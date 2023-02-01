d912373fca910126774ad48744142902a1479199
heeyunkwon <hkite08@gmail.com>
2023-02-01 17:48:36 +0900

    Initial Commit

---
+++ UserGenerator/UserGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserGeneratorApp.swift
+//  UserGenerator
+//
+//  Created by netid on 2023/02/01.
+//
+
+import SwiftUI
+
+@main
+struct UserGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
