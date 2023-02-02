1380b896bba635b2b1436e286530257599445b2e
heeyunkwon <hkite08@gmail.com>
2023-02-02 11:04:27 +0900

    Update ContentView.swift

--- UserGenerator/ContentView.swift
+++ UserGenerator/ContentView.swift
@@ -7,15 +7,95 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Menu: View {
+    @Binding var title: String
+    @Binding var info: String
+    
+    var imgName = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button {
+            btnAction(img: imgName)
+        } label: {
+            Image(systemName: imgName)
+                .frame(width: 30, height: 20)
         }
-        .padding()
+    }
+    func btnAction(img: String) {
+        switch img{
+        case "person":
+            title = "Hi, My name is"
+            info = "Kwon Hee Yun"
+        case "mail":
+            title = "My email address is"
+            info = "heeyun@example.com"
+        case "calendar":
+            title = "My birthday is"
+            info = "8/16/2000"
+        case "map":
+            title = "My address is"
+            info = "netid"
+        case "phone":
+            title = "My phone number is"
+            info = "010-1234-5678"
+        case "lock":
+            title = "My password is"
+            info = "mylife"
+        default:
+            break
+        }
+    }
+}
+
+struct MyBtnStyle: ButtonStyle {
+    var btnColor: Color = .green
+    var btnRadius: CGFloat = 5
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .padding(.horizontal, 30)
+            .padding(10)
+            .background(RoundedRectangle(cornerRadius: btnRadius).fill(btnColor))
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+    }
+}
+
+struct ContentView: View {
+    @State private var title = "Hi, My name is"
+    @State private var info = "Hong Gil Dong"
+    private let btnImg = [
+        "person", "mail", "calendar",
+        "map", "phone", "lock",
+    ]
+    var body: some View {
+        VStack{
+            //프로필 이미지
+            Circle()
+                .frame(width: 150, height: 150)
+            //유저 이름
+            Text(title)
+                .foregroundColor(.gray)
+                .padding(.top, 10)
+            Text(info)
+                .font(.largeTitle)
+            //메뉴버튼
+            HStack{
+                ForEach(0..<6, id: \.self) { i in
+                    Menu(title: $title, info: $info, imgName: btnImg[i])
+                }
+            }
+            .padding(.bottom, 10)
+            //새로운 유저 불러오는 버튼
+            Button {
+                //todo: "New" btton action
+            } label: {
+                Text("New")
+            }
+            .buttonStyle(MyBtnStyle())
+        }
+        .padding(.horizontal, 100)
+        .padding(50)
     }
 }
 
